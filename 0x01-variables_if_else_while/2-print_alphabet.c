8eebb8552db3fb14bbc2937f2ad969cecfe65950
MakidAmare <makidaamare@gmail.com>
2022-04-08 18:12:44 -0700

    dd

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: Print the lowercase alphabet
+ * Reture: 0
+ */
+int main(void)
+{
+	int c;
+	for (c = 0; c < 26; ++c)
+		putchar('a' + c);
+	putchar('\n');
+	return (0);
+}
