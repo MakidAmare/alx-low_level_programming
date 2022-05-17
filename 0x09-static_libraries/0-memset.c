652888679d82ab76520759a3b18154650bc7f7b8
MakidAmare <102348668+MakidAmare@users.noreply.github.com>
2022-05-18 00:50:34 +0530

    Update 0-memset.c

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,16 +1,17 @@
+#include <string.h>
 #include "main.h"
+
 /**
- * _memcpy - copies memory area,
- * @dest: destination memory area.
- * @src: source memory area.
- * @n: bytes filled.
- * Return: the pointer to dest.
+ * *_memset -  fills string
+ *
+ * @s: string pointer
+ * @b: replace char
+ * @n: bytes number
+ * Return: char*
  */
-char *_memset(char *s, char b, unsigned int n);
-	unsigned int i;
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(s, b, n);
 
-	for (i = 0; i < n; i++)
-		*(dest + i) =  *(src + i);
-
-	return (dest);
+	return (s);
 }
